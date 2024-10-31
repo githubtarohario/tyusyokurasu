a2f7d088cab0ff0a069c01afa20e9b4fd9893028
PCUSER <taka_ken2000@yahoo.co.jp>
2024-11-01 02:36:42 +0900

    プロジェクト ファイルを追加します。

---
+++ tyusyokurasu/tyusyokurasu.cpp
@@ -0,0 +1,74 @@
+﻿// tyusyokurasu.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+/*
+純粋仮想関数のサンプル
+
+*/
+#include <iostream>
+using namespace std;
+class IAnimal {
+public:
+    virtual void bark()=0;
+    virtual void run() = 0;
+    virtual ~IAnimal() {}
+
+};
+class Dog :public IAnimal {
+public:
+    void bark() {
+        cout << "WAN WAN" << endl;
+
+    }
+    void run() {
+
+
+    }
+    ~Dog() {
+
+
+    }
+
+};
+class Cat :public IAnimal {
+public:
+    void bark() {
+
+        cout << "NYAN NYAN " << endl;
+    }
+    void run() {
+
+        
+    }
+    ~Cat() {
+
+
+    }
+
+};
+
+
+
+
+int main()
+{
+    
+    IAnimal* s[] = {
+      new Dog,
+      new Cat,
+      //nullptr
+    };
+    for (const auto& p : s) {
+        p->run();
+        p->bark();
+ 
+    }
+
+
+
+
+
+}
+
+
+
+
